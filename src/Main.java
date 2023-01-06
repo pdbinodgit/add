c573e6204951de61eec4b210e78581816426e71f
Bishnu Prasad pandey <pandeybinod2075@gmail.com>
2023-01-06 14:58:47 +0545

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a= 2;
+         int b= 3;
+         int  sum =a+b;
+        System.out.println(sum);
+        
+
+
+    }
+}
